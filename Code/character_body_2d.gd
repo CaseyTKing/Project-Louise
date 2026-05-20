ea9576fb4b1b4c4b48f7df091a53bfcdef6796e8
LeonRodWyatt <40241573+SnakeEyesWyatt@users.noreply.github.com>
2026-05-20 19:02:07 +0100

    added sprite change on direction change

--- Code/character_body_2d.gd
+++ Code/character_body_2d.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 var inventory: Array[String] = []
 @export var max_health: float = 100
 var health: int = max_health
+@export var animation_tree : AnimationTree
+var playback : AnimationNodeStateMachinePlayback
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
+	select_animation()
+	update_animation_parameters()
 
 func add_item(item_name: String) -> void:
 	inventory.append(item_name)
@@ -23,3 +27,15 @@ func heal(amount: int) -> void:
 	
 func die() -> void:
 	queue_free()
+	
+func update_animation_parameters():
+	animation_tree["parameters/Walking/blend_position"] = velocity
+	
+func _ready():
+	playback = animation_tree["parameters/playback"]
+	
+func select_animation():
+	if velocity == Vector2.ZERO:
+		playback.travel("Idle")
+	else:
+		playback.travel("Walking")
